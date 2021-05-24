0c6a080ad4d04df16f8177077dd574796182d4ac
juma-moringa <allan.juma@student.moringaschool.com>
2021-05-24 13:45:55 +0300

    updates on the environment file

--- src/app/repos.service.ts
+++ src/app/repos.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environments';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
